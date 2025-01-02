4a02b3743facccf2013a4207e27591e9a76c6bfb
javilopezdiez <javilopezdiez@protonmail.com>
2025-01-02 11:38:01 +0100

    01012025-13

--- architect.sh
+++ architect.sh
@@ -1,6 +1,6 @@
 #/!bin/bash
 # Command for execution
-# sudo bash <(curl -L https://raw.githubusercontent.com/javilopezdiez/architect/main/architect.sh) 2> architect.log
+# curl -L https://raw.githubusercontent.com/javilopezdiez/architect/main/architect.sh && sudo bash architect.sh > architect.log
 
 ##### VARIABLES #####
 USER=loncelot
@@ -20,7 +20,7 @@ if [[ $EUID -ne 0 ]]; then
 fi
 
 echo "INSTALL STARTED..."
-##### VARIABLES #####
+##### DEPENDENCIES #####
 echo "Installing numfmt for partitioning..."
 pacman -Sy --noconfirm --needed numfmt
 
@@ -111,53 +111,6 @@ echo "Generating fstab..."
 genfstab -U /mnt >> /mnt/etc/fstab
 
 # Changing the root directory to /mnt
-arch-chroot /mnt
-
-# Grub configuration
-grub-install --target=i386-pc $DISK
-grub-mkconfig -o /boot/grub/grub.cfg
-
-# Root config
-# echo "Insert root passwd"
-# passwd
-
-# User config
-echo "Adding user $USER..."
-useradd -m -g wheel $USER
-echo "Insert your passwd"
-# passwd $USER
-sed -i 's/^# %wheel ALL=(ALL:ALL) NOPASSWD: ALL/%wheel ALL=(ALL:ALL) NOPASSWD: ALL/' /etc/sudoers
-
-# Host config
-echo "Naming host $HOSTNAME..."
-echo "$HOSTNAME" > /etc/hostname
-echo "127.0.0.1 localhost" > /etc/hosts
-
-# Localization config
-echo "Configuring localization..."
-echo "KEYMAP=es" > /etc/vconsole.conf
-localectl set-keymap es
-echo -e "LANG=en_US.UTF-8\nLC_NUMERIC=es_ES.UTF-8\nLC_TIME=es_ES.UTF-8\nLC_MONETARY=es_ES.UTF-8\nLC_PAPER=es_ES.UTF-8\nLC_NAME=es_ES.UTF-8\nLC_ADDRESS=es_ES.UTF-8\nLC_TELEPHONE=es_ES.UTF-8\nLC_MEASUREMENT=es_ES.UTF-8\nLC_IDENTIFICATION=es_ES.UTF-8" > /etc/locale.conf
-sed -i 's/^#en_US.UTF-8 UTF-8/en_US.UTF-8 UTF-8/' /etc/locale.gen
-sed -i 's/^#en_US ISO-8859-1/en_US ISO-8859-1/' /etc/locale.gen
-sed -i 's/^#es_ES.UTF-8 UTF-8/es_ES.UTF-8 UTF-8/' /etc/locale.gen
-sed -i 's/^#es_ES ISO-8859-1/es_ES ISO-8859-1/' /etc/locale.gen
-locale-gen
-ln -sf /usr/share/zoneinfo/Europe/Madrid /etc/localtime
-
-# Package manager update
-sudo pacman -Syu --noconfirm
-
-# AUR helper install
-echo "Installing yay AUR helper..."
-mkdir /tmp/yay && cd /tmp/yay
-curl -OJ 'https://aur.archlinux.org/cgit/aur.git/plain/PKGBUILD?h=yay'  
-makepkg -si --noconfirm
-rm -rf /tmp/yay
-
-# Desktop environment isntall
-echo "Installing xfce Desktop environment..."
-sudo pacman -S xorg xfce4 xfce4-goodies lightdm lightdm-gtk-greeter --noconfirm
-sudo systemctl enable lightdm
+arch-chroot /mnt bash -c "curl -L https://raw.githubusercontent.com/javilopezdiez/architect/main/architect-setup.sh | bash" > /mnt/architect-setup.log
 
 echo "INSTALL COMPLETED..."
