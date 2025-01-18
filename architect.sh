187bbf74086489fce0526c4215b3fdb97a63df4c
javilopezdiez <javilopezdiez@protonmail.com>
2025-01-18 16:44:51 +0100

    +

--- architect.sh
+++ architect.sh
@@ -1,31 +1,31 @@
 #!/bin/bash
 
 # Installing DEPENDENCIES
-echo "Installing git."
-pacman -Sy --noconfirm --needed git glibc numfmt
+echo "Installing git. test"
+# pacman -Sy --noconfirm --needed git glibc numfmt
 
-# Cloning scripts
-echo "Cloning the architect script"
-git clone https://github.com/javilopezdiez/architect
+# # Cloning scripts
+# echo "Cloning the architect script"
+# git clone https://github.com/javilopezdiez/architect
 
-# SCRIPTS
-cd $HOME/architect
-echo -ne "--------------------------0- INSTALL STARTED...----------------"
-( bash \
-    $HOME/architect/architect-install.sh )|& \
-    tee architect-install.log
+# # SCRIPTS
+# cd $HOME/architect
+# echo -ne "--------------------------0- INSTALL STARTED...----------------"
+# ( bash \
+#     $HOME/architect/architect-install.sh )|& \
+#     tee architect-install.log
 
-echo -ne "--------------------------1- SETUP STARTED...------------------"
-( arch-chroot /mnt \
-    $HOME/architect/architect-setup.sh )|& \
-    tee architect-setup.log
+# echo -ne "--------------------------1- SETUP STARTED...------------------"
+# ( arch-chroot /mnt \
+#     $HOME/architect/architect-setup.sh )|& \
+#     tee architect-setup.log
 
-echo -ne "--------------------------2- POST-ISNTALL PKG STARTED...-------"
-( arch-chroot /mnt /usr/bin/runuser -u $USER -- \
-    $HOME/architect/architect-pkg.sh )|& \
-    tee architect-pkg.log
+# echo -ne "--------------------------2- POST-ISNTALL PKG STARTED...-------"
+# ( arch-chroot /mnt /usr/bin/runuser -u $USER -- \
+#     $HOME/architect/architect-pkg.sh )|& \
+#     tee architect-pkg.log
 
-echo -ne "--------------------------2- POST-ISNTALL COMPLETED...---------"
+# echo -ne "--------------------------2- POST-ISNTALL COMPLETED...---------"
 
-# LOGS
-cp -v *.log /mnt/home/$USER
+# # LOGS
+# cp -v *.log /mnt/home/$USER
