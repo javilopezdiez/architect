8c9a63a060dbc5f99bcc97fb9967f4bec7d6894c
javilopezdiez <javilopezdiez@protonmail.com>
2025-02-06 13:23:05 +0100

    todo mybackup

--- config/home/loncelot/.local/bin/mybackup.sh
+++ config/home/loncelot/.local/bin/mybackup.sh
@@ -1,188 +1,173 @@
 #!/bin/bash
 
-DU="Destination of backup"
-BU="Backup source files to destination"
-GU="Backup source files to git repo"
+backup_string="Backup source files to destination"
+git_string="Backup source files to git repo"
 
 # Default directories
-B="/run/media/$USER/nail/backup"
-G="$HOME/Workspace/architect/config"
+backup_directory="/run/media/$USER/nail/backup"
+	# I made the disk with a label like this
+		# sudo fdisk /dev/mmcblk0
+			# d -> all the partitions
+			# n -> 1 partition deleting signature
+			# w write changes
+		# sudo mkfs.ext4 -L nail /dev/mmcblk0p1
+git_directory="$HOME/Workspace/architect/config"
 
-source_dirs=(
-    "$HOME/.profile"
-    "$HOME/.bashrc"
-    # "$HOME/.face"
+file_patterns=(
+	# TODO cargo already in .bashrc
+	# "$HOME/.profile"
 
-    # "$HOME/.local/bin"
-    # "$HOME/.local/bin/mysrc"
-    # "$HOME/.local/bin/my*.sh"
-    "$HOME/.themes"
+	"$HOME/.bashrc"
+	"$HOME/.local/bin/my*"
 
-    "$HOME/.config/xfce4"
-    "$HOME/.config/devilspie2"
-    "$HOME/.config/Code/User/*.json"
-    "$HOME/.config/Thunar"
-    "$HOME/.config/autostart/my*.desktop"
-    # "$HOME/.config/autostart/mykeyboard.desktop"
-    # "$HOME/.config/autostart/mydevilspie2.desktop"
-    # "$HOME/.config/autostart/my*.desktop"
-    "$HOME/.config/mimeapps.list"
-    # "$HOME/.config/myicon"
+	"$HOME/.config/xfce4"
+	"$HOME/.config/devilspie2"
+	"$HOME/.config/Code/User/*.json"
+	"$HOME/.config/Thunar"
+	"$HOME/.config/autostart/my*.desktop"
+	"$HOME/.config/mimeapps.list"
 
-    "$HOME/Pictures/wallpapers/art/bierstadt"
-    "$HOME/Pictures/wallpapers/mybackground.png"
+	"$HOME/.face"
+	"$HOME/.themes"
+	"$HOME/Pictures/wallpapers/art/bierstadt"
+	"$HOME/Pictures/wallpapers/mybackground.png"
 
-    # TODO if changes mkconfig
-    "/etc/default/grub"
-    "/etc/lightdm/lightdm-gtk-greeter.conf"
-    # "/etc/skel/.config/xfce4/xfconf/xfce-perchannel-xml/xfce4-desktop.xml"
-    # TODO ownership root
-    # might it be sudo chmod 440 /etc/sudoers
-    # "/etc/sudoers"
-    # "/etc/xdg"
+	# TODO ownership root
+	# might it be sudo chmod 440 /etc/sudoers
+	# "/etc/sudoers"
 
-    # "/usr/lib/vmware/view/dct/configFiles"
-    "/usr/share/backgrounds/my-lockscreen.jpeg"
-    "/usr/share/backgrounds/xfce/xfce-shapes.svg"
-    "/usr/share/backgrounds/xfce/xfce-x.svg"
+	# TODO if changes mkconfig
+	"/etc/default/grub"
+
+	"/etc/lightdm/lightdm-gtk-greeter.conf"
+
+	"/usr/share/backgrounds/my-lockscreen.jpeg"
+	"/usr/share/backgrounds/xfce/xfce-shapes.svg"
+	"/usr/share/backgrounds/xfce/xfce-x.svg"
 )
 
 main() {
-    if ! command -v rsync &> /dev/null; then
-        echo "rsync is not installed. Installing..."
-        if ! sudo pacman -S rsync; then
-            echo "Failed to install rsync. Exiting."
-            exit 1
-        fi
-    fi
-
-    # if ! pacman -Q bash-completion &> /dev/null; then
-    #     echo "bash-completion is not installed. Installing..."
-    #     if ! sudo pacman -S bash-completion; then
-    #         echo "Failed to install bash-completion. Exiting."
-    #         exit 1
-    #     fi
-    # fi
-
-    parameters "$@"
+	if ! command -v rsync &> /dev/null; then
+		echo "rsync is not installed. Installing..."
+		if ! sudo pacman -S rsync; then
+			echo "Failed to install rsync. Exiting."
+			exit 1
+		fi
+	fi
+	parameters "$@"
 }
 
 parameters() {
-    dirParameter $@
-    actionParameters $@
+	dirParameter $@
+	actionParameters $@
 }
 dirParameter() {
-    # Resetting the dir in case there is
-    while [[ $# -gt 0 ]]; do
-        case "$1" in
-            -d*|--directory*)
-                B=$(get_directory_argument "$1")
-                echo $DU set to $B
-                ;;
-            -r|--restore);;
-            -b|--backup);;
-            -g|--git);;
-            *)
-                usage
-                exit 1
-                ;;
-        esac
-        shift
-    done
+	# Resetting the dir in case there is
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			-d*|--directory*)
+				backup_directory=$(get_directory_argument "$1")
+				echo Backup directory set to $backup_directory
+				;;
+			-r|--restore);;
+			-b|--backup);;
+			-g|--git);;
+			*)
+				usage
+				exit 1
+				;;
+		esac
+		shift
+	done
 }
 actionParameters() {
-    # Copying
-    while [[ $# -gt 0 ]]; do
-        case "$1" in
-            -d*|--directory*)
-                ;;
-            -r|--restore)
-                direction="restore"
-                copy $B
-                exit
-                ;;
-            -b|--backup)
-                echo $BU;
-                direction="backup"
-                copy $B
-                ;;
-            -g|--git)
-                echo $GU;
-                direction="backup"
-                copy $G
-                ;;
-            *)
-                usage
-                exit 1
-                ;;
-        esac
-        shift
-    done
+	# Copying
+	while [[ $# -gt 0 ]]; do
+		case "$1" in
+			-d*|--directory*)
+				;;
+			-r|--restore)
+				direction="restore"
+				copy $backup_directory
+				exit
+				;;
+			-b|--backup)
+				echo $backup_string;
+				direction="backup"
+				copy $backup_directory
+				;;
+			-g|--git)
+				echo $git_string;
+				direction="backup"
+				copy $git_directory
+				;;
+			*)
+				usage
+				exit 1
+				;;
+		esac
+		shift
+	done
 }
 
+# Returns the directory in d=directory
 get_directory_argument() {
-    if [[ "$1" == *"="* ]]; then
-        echo "${1#*=}"
-    else
-        usage
-        exit 1
-    fi
+	if [[ "$1" == *"="* ]]; then
+		echo "${1#*=}"
+	else
+		usage
+		exit 1
+	fi
 }
 
 usage() {
-    echo "Usage: $0 [-b|-g|-r]"
-    echo ""
-    echo "  -d, --destination=<directory>   Specify the directory to use for backup or restore."
-    echo "                                  The directory should be provided in the format: -d=<directory>"
-    echo "  -b, --backup                    $BU"
-    echo "  -g, --git                       $GU"
-    echo "  -r, --restore                   Restore destination directories to source directories (this will terminate the script after execution)"
-    echo "  --help                          Display this help message"
+	echo "Usage: $0 [-b|-g|-r]"
+	echo ""
+	echo "  -d, --destination=<directory>   Specify the directory to use for backup or restore."
+	echo "                                  The directory should be provided in the format: -d=<directory>"
+	echo "  -b, --backup                    $backup_string"
+	echo "  -g, --git                       $git_string"
+	echo "  -r, --restore                   Restore destination directories to source directories (this will terminate the script after execution)"
+	echo "  --help                          Display this help message"
 }
 
 copy() {
-    C="$1"
-    CHOME=$C$HOME
+	location="$1"
+	locationHOME="$location$HOME"
 
-    echo "The backup files are/are tobe in: $C"
-    
-    if [[ "$direction" == "backup" ]]; then
-        for source_dir in "${source_dirs[@]}"; do
-            for expanded_source in $source_dir; do
-                if [[ "$expanded_source" == $HOME* ]]; then
-                    backup_dir="${expanded_source/$HOME/$CHOME}"
-                else
-                    backup_dir="${C}${expanded_source}"
-                fi
-                sudo mkdir -p "$(dirname "$backup_dir")"
-                if [[ -d "$expanded_source" ]]; then
-                    sudo rsync -ah --info=progress2 "$expanded_source/" "$backup_dir"
-                else
-                    sudo rsync -ah --info=progress2 "$expanded_source" "$backup_dir"
-                fi
-            done
-        done
-    elif [[ "$direction" == "restore" ]]; then
-        for source_dir in "${source_dirs[@]}"; do
-            for expanded_source in $source_dir; do
-                base_source_dir="${expanded_source%/*}" 
-                if [[ "$expanded_source" == $HOME* ]]; then
-                    backup_dir="${base_source_dir/$HOME/$CHOME}"
-                else
-                    backup_dir="${C}${base_source_dir}"
-                fi
-                sudo mkdir -p "$(dirname "$base_source_dir")"
-                if [[ -d "$backup_dir" ]]; then
-                    sudo rsync -ah --info=progress2 "$backup_dir/" "$base_source_dir"
-                else
-                    sudo rsync -ah --info=progress2 "$backup_dir" "$base_source_dir"
-                fi
-            done
-        done
-    else
-        usage
-    fi
+	echo "The files will be backed up/restored from/to: $location"
+	for file_pattern in "${file_patterns[@]}"; do
+		# Expand file patterns to match actual files (e.g., *.extension)
+		for full_path in $file_pattern; do
+			# Get parent directory from full file path
+			parent_directory="${full_path%/*}"
+			# Substitude HOME for locationHOME
+			if [[ "$parent_directory" == $HOME* ]]; then
+				backup_path="${parent_directory/$HOME/$locationHOME}"
+			else
+				backup_path="${location}${parent_directory}"
+			fi
+			if [[ "$direction" == "backup" ]]; then
+				copy_content "$full_path" "$backup_path"
+			elif [[ "$direction" == "restore" ]]; then
+				copy_content "$backup_path" "$parent_directory"
+			fi
+		done
+	done
 }
+
+# If source is a dir, copy its contents into target
+copy_content() {
+	source="$1"
+	target="$2"
+	sudo mkdir -p "$(dirname "$target")"
+	if [[ -d "$source_path" ]]; then
+		sudo rsync -ah --info=progress2 "$source/" "$target"
+	else
+		sudo rsync -ah --info=progress2 "$source" "$target"
+	fi
+}
+
 
 # Execute the main function
 main "$@"
